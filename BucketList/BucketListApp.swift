5c0b5e8f08c32b494bb8881b5eac812ecd4bb50a
Kirill <K-baranov@outlook.com>
2024-01-23 22:20:11 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kirill Baranov on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
